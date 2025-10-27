dc787edcdbde12623a8848e281e1fdc5ca3db169
MIkhail <dmihail528@gmail.com>
2025-10-27 12:21:33 +0300

    расчет окупаемости, фикс вывода ошибок

--- src/app/actions/updatePaybackData.js
+++ src/app/actions/updatePaybackData.js
@@ -3,14 +3,13 @@ import { getDb } from "@/db/index";
 import { mainInformation } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function saveBomAndServicesAction(id, bomData, servicesData) {
+export async function updatePaybackDataAction(id, paybackData) {
   const db = getDb();
 
   await db
     .update(mainInformation)
     .set({
-      bomData: bomData,
-      servicesData: servicesData,
+      paybackData: paybackData,
     })
     .where(eq(mainInformation.id, id));
 
